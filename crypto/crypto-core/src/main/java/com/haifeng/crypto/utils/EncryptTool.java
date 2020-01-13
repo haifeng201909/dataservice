430fe902e1047928872c67541af19a192aac824c
admin <1726711198@qq.com>
2020-01-13 22:10:02 +0800

    update

--- crypto/crypto-core/src/main/java/com/haifeng/crypto/utils/EncryptTool.java
+++ crypto/crypto-core/src/main/java/com/haifeng/crypto/utils/EncryptTool.java
@@ -1,7 +1,14 @@
 package com.haifeng.crypto.utils;
 
-public class EncryptTool {
-    public static void main(String[] args){
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.misc.BASE64Encoder;
 
+public class EncryptTool {
+    private static final Logger LOG = LoggerFactory.getLogger(EncryptTool.class);
+    public static void main(String[] args){
+        byte[] encrypted = {1,2,3,4};
+        String result = new BASE64Encoder().encode(encrypted);
+        LOG.info(result);
     }
 }
